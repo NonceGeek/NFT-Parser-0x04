5a41326b53082576599cbffa732089699ed14a5a
wangningbo <2536935847@qq.com>
2021-07-18 10:25:13 +0800

    改成https

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config) {
 
   const instance = axios.create({
-    baseURL: 'http://47.106.10.136:5000',
+    baseURL: 'https://taishang.cnmf.net.cn:5000',
     timeout: 5000
   })
   return instance(config)
